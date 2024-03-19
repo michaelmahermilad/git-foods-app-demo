5a22233fbd070619136619399a78738c0ecf77b1
Michael Maher <michael.maher@vas-eg.net>
2024-03-20 01:27:32 +0400

    yes

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import "./Nav.css"
+import logo1 from "../Images/logo1.png"
 function index() {
   return (
     <div style={{height:"5rem"}}>
       <div className="cont">
-        <div className="logo">
-
-
-        </div>
-        <div className="items">
-        <a href="/" className="item">
+     
+<img src={logo1} style={{height:"4rem",float:"left",marginTop:".8rem"}}/>
 
         
-        </a>
+        <div className="items"style={{height:"5rem",float:"right"}}>
+        <a href="/" className="item">About</a>
+        <a href="/" className="item">Services</a>
+        <a href="/" className="item">Farms</a>
+        <a href="/" className="item">Gallery</a>
+        <a href="/" className="item">Contact us</a>
+      
         </div>
       </div>
 
